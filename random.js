19b1e8864ae96389352fa47ffd5e3d644aadfd43
DV7 <RisingCoderVajahat@gmail.com>
2025-05-30 16:05:02 +0530

    Update random.js

--- random.js
+++ random.js
@@ -4,17 +4,16 @@ let counter = 0;
 button.addEventListener('click', function () {
     if (counter === 0) {
         div.style.backgroundColor = 'yellow';
-        button.style.backgroundColor = 'red';
-        button.innerText = 'Switch OFF bulb!';
+        button.style.backgroundColor = 'rgb(252, 71, 0)';
+        button.innerHTML = '<strong>Switch OFF bulb!</strong>';
         counter =1;
     } else if (counter === 1) {
         div.style.backgroundColor = 'transparent';
-        button.style.backgroundColor = 'green';
-        button.innerText = 'Switch ON bulb!';
+        button.style.backgroundColor = 'lawngreen';
+        button.innerHTML = '<strong>Switch ON bulb!</strong>';
         counter =0;
     }
 });
-
 
 
 
